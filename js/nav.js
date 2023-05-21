f33c0eb37d0d000bff28890092c9b709c8b4c0de
Juan Ignacio Velesquen Saenz <juanivelesquen@gmail.com>
2023-05-21 17:56:56 -0300

    update nav

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,7 @@
 const primaryNav = document.querySelector(".primary-navigation");
 const navtoggle = document.querySelector(".mobile-nav-toggle");
 const navLink = document.querySelectorAll(".nav__link");
+const links = Array.from(primaryNav.children);
 
 navtoggle.addEventListener("click", () => {
     const visibility = primaryNav.getAttribute("data-visible");
@@ -19,7 +20,10 @@ navtoggle.addEventListener("click", () => {
 for(let i = 0; i < navLink.length;i++)
 {
     navLink[i].addEventListener("click", () => {
+        let currenLink = primaryNav.querySelector(".nav__link-active");
         primaryNav.setAttribute("data-visible","false");
         navtoggle.setAttribute("aria-expanded","false");
+        currenLink.classList.remove("nav__link-active");
+        navLink[i].classList.add("nav__link-active");
     });
 }
