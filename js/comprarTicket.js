761b3b2560548cf82c0dcf682b72f916d54e3eb7
Juan Ignacio Velesquen Saenz <juanivelesquen@gmail.com>
2023-05-30 16:45:19 -0300

    validacion de formulario

--- js/comprarTicket.js
+++ js/comprarTicket.js
@@ -1,16 +1,42 @@
-const cant = document.getElementById("cantidad");
 const select = document.getElementById("categoria");
 const resumen = document.getElementById("resumen");
 const borrar = document.getElementById("borrar");
 
-resumen.addEventListener("click", () =>{
-    let categoria = select.value;
-    let cantidad = parseInt(cant.value);
-    let suma = 0;
-    console.log(cantidad);
-    if(cantidad == NaN){
-        cantidad = 0;
+//validacion
+function validar(cant){
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const correo = document.getElementById("correo").value;
+    var expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if(nombre.trim().length == 0){
+        alert("Error: Ingrese un nombre");
+        return false;
     }
+    if(apellido.trim().length == 0){
+        alert("Error: Ingrese un apellido ");
+        return false;
+    }
+    if( !expr.test(correo) ){
+        alert("Error: Ingrese un correo valido");
+        return false;
+    }
+    if(cant.trim().length == 0){
+        alert("Error: Ingrese cantidad de entradas");
+        return false;
+    }
+    return true;
+}
+
+//calcular la cantidad total del precio de la/s entrada/s
+resumen.addEventListener("click", () =>{
+    const cant = document.getElementById("cantidad").value;
+    let v = validar(cant);
+    if(!(v)){
+        return;
+    }
+    let categoria = select.value;
+    let cantidad = parseInt(cant);
+    let suma = 0;
     if(categoria == "estudiante"){
         suma = cantidad * 200 * 0.2;
     }else if(categoria == "trainee"){
@@ -20,7 +46,7 @@ resumen.addEventListener("click", () =>{
     }
     document.getElementById("total-pagar").innerHTML = "$" + suma;
 })
-
+//restablecer todos los datos ingresados
 borrar.addEventListener("click", () =>{
     document.getElementById("cantidad").value = null;
     document.getElementById("categoria").value = "estudiante";
